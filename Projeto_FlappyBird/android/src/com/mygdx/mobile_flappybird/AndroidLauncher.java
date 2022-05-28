fcedd02e6d1e86abfda697154a05feba8e11b6e7
stephany g sodre <69463672+stephanysodre@users.noreply.github.com>
2022-05-28 18:41:35 -0300

    12
    12

--- Projeto_FlappyBird/android/src/com/mygdx/mobile_flappybird/AndroidLauncher.java
+++ Projeto_FlappyBird/android/src/com/mygdx/mobile_flappybird/AndroidLauncher.java
@@ -1,16 +1,16 @@
-package com.mygdx.game;
+package com.mygdx.mobile_flappybird;
 
 import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.jogo;
+import com.mygdx.mobile_flappybird.game;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new jogo(), config);
+		initialize(new game(), config);
 	}
 }
--- Projeto_FlappyBird/core/src/com/mygdx/mobile_flappybird/game.java
+++ Projeto_FlappyBird/core/src/com/mygdx/mobile_flappybird/game.java
@@ -1,4 +1,4 @@
-package com.mygdx.game;
+package com.mygdx.mobile_flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
@@ -14,14 +14,14 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Intersector;
-import com.badlogic.gdx.utils.Logger;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
-import com.sun.tools.javac.util.Log;
 
 import java.util.Random;
 
-public class jogo extends ApplicationAdapter {
+public class game extends ApplicationAdapter
+{
+	//
 	private SpriteBatch batch;
 	private Texture[] passaros;
 	private Texture fundo;
@@ -32,18 +32,15 @@ public class jogo extends ApplicationAdapter {
 	private Texture moeda1;
 	private Texture moeda2;
 	private Texture moedaAtual;
-	float escalamoeda = .25f;
-	float posicaoMoedaX;
-	float posicaoMoedaY;
-	float valorMoeda1 = 5;
-	float valormoeda2 = 10;
 
+	//
 	private ShapeRenderer shapeRenderer;
 	private Circle circuloPassaro;
 	private Rectangle retanguloCanoCima;
 	private Rectangle retanguloCanoBaixo;
 	private Circle circuloMoeda;
 
+	//
 	private float larguraDispositivo;
 	private float alturaDipositivo;
 	private float variacao = 0;
@@ -57,23 +54,35 @@ public class jogo extends ApplicationAdapter {
 	private int pontuacaoMaxima=0;
 	private boolean passouCano=false;
 	private int estadoJogo = 0;
-	private float posicaoHorizontalPassaro = 0;
+	private float posicaoHorizontalPassaro = 50;
+	private float escalaPassaro = .5f;
+	private float escalaMoeda = .25f;
+	private float posicaoMoedaX;
+	private float posicaoMoedaY;
+	private float valorMoeda1 = 5;
+	private float valorMoeda2 = 10;
 
-	BitmapFont textoPontuacao;
-	BitmapFont textoReiniciar;
-	BitmapFont textoMelhorPontuacao;
+	//
+	private BitmapFont textoPontuacao;
+	private BitmapFont textoReiniciar;
+	private BitmapFont textoMelhorPontuacao;
 
-	Sound somVoando;
-	Sound somColisao;
-	Sound somPontuacao;
+	//
+	private Sound somVoando;
+	private Sound somColisao;
+	private Sound somPontuacao;
+	private Sound somMoeda;
 
-	Preferences preferencias;
+	//
+	private Preferences preferencias;
 
+	//
 	private OrthographicCamera camera;
 	private Viewport viewport;
 	private final float VIRTUAL_WIDTH = 720;
 	private final float VIRTUAL_HEIGHT = 1280;
 
+	//
 	@Override
 	public void create()
 	{
@@ -81,19 +90,22 @@ public class jogo extends ApplicationAdapter {
 		inicializaObjetos();
 	}
 
+	//
 	@Override
 	public void render()
 	{
+		//
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
+		//
 		verificarEstadoJogo();
 		validarPontos();
-		desenharTexturas();
+		desenharCena();
+		desenharInterface();
 		detectarColisoes();
 	}
 
-
-
+	//
 	private void inicializarTexturas()
 	{
 		passaros = new Texture[3];
@@ -106,12 +118,13 @@ public class jogo extends ApplicationAdapter {
 		canoBaixo = new Texture("cano_baixo_maior.png");
 		canoTopo = new Texture("cano_topo_maior.png");
 		gameOver = new Texture("game_over.png");
-		moeda1 = new Texture("moedaouro.png");
-		moeda2 = new Texture("moedaprata.png");
+		moeda1 = new Texture("moedaprata.png");
+		moeda2 = new Texture("moedaouro.png");
 
 		moedaAtual = moeda2;
 	}
 
+	//
 	private  void inicializaObjetos()
 	{
 		batch = new SpriteBatch();
@@ -146,6 +159,7 @@ public class jogo extends ApplicationAdapter {
 		somVoando = Gdx.audio.newSound(Gdx.files.internal("som_asa.wav"));
 		somColisao = Gdx.audio.newSound(Gdx.files.internal("som_batida.wav"));
 		somPontuacao = Gdx.audio.newSound(Gdx.files.internal("som_pontos.wav"));
+		somMoeda = Gdx.audio.newSound(Gdx.files.internal("som_moeda.wav"));
 
 		preferencias = Gdx.app.getPreferences("flappyBird");
 		pontuacaoMaxima = preferencias.getInteger("pontuacaoMaxima",0);
@@ -155,6 +169,7 @@ public class jogo extends ApplicationAdapter {
 		viewport = new StretchViewport(VIRTUAL_WIDTH,VIRTUAL_HEIGHT, camera);
 	}
 
+	//
 	private  void verificarEstadoJogo()
 	{
 		boolean toqueTela = Gdx.input.justTouched();
@@ -185,6 +200,11 @@ public class jogo extends ApplicationAdapter {
 				posicaoCanoVertical = random.nextInt(400) - 200;
 				passouCano = false;
 			}
+			// Reseta moeda
+			if (posicaoMoedaX < -moedaAtual.getWidth() / 2 * escalaMoeda)
+			{
+				resetaMoeda();
+			}
 			if (posicaoInicialVerticalPassaro > 0 || toqueTela)
 			{
 				posicaoInicialVerticalPassaro = posicaoInicialVerticalPassaro - gravidade;
@@ -207,46 +227,48 @@ public class jogo extends ApplicationAdapter {
 				estadoJogo = 0;
 				pontos = 0;
 				gravidade = 0;
-				posicaoHorizontalPassaro = 0;
+				posicaoHorizontalPassaro = 50;
 				posicaoInicialVerticalPassaro = alturaDipositivo / 2;
 				posicaoCanoHorizontal = larguraDispositivo;
+				resetaMoeda();
 			}
 		}
 	}
 
+	//
 	private void detectarColisoes()
 	{
 		// Cria colisor passaro
 		circuloPassaro.set
 		(
-			50 + posicaoHorizontalPassaro + passaros[0].getWidth() / 2 / 2,
-			posicaoInicialVerticalPassaro + passaros[0].getHeight() / 2 / 2,
-			(passaros[0].getWidth() / 2) / 2
+			posicaoHorizontalPassaro + passaros[0].getWidth() * escalaPassaro / 2,
+			posicaoInicialVerticalPassaro + passaros[0].getHeight() * escalaPassaro / 2,
+			(passaros[0].getHeight()  * escalaPassaro) / 2
 		);
 
 		// Cria colisor moeda
 		circuloMoeda.set
 		(
-			posicaoMoedaX - moedaAtual.getWidth()/2 * escalamoeda,
-			posicaoMoedaY - moedaAtual.getHeight()/2 * escalamoeda,
-			(moedaAtual.getWidth() * escalamoeda) / 2
+			posicaoMoedaX - ((moedaAtual.getWidth() * escalaMoeda) / 2),
+			posicaoMoedaY - ((moedaAtual.getHeight() * escalaMoeda) / 2),
+			(moedaAtual.getWidth() * escalaMoeda) / 2
 		);
 
 		retanguloCanoBaixo.set
-		(
-			posicaoCanoHorizontal,
-			alturaDipositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + posicaoCanoVertical,
-			canoBaixo.getWidth(),
-			canoBaixo.getHeight()
-		);
+				(
+						posicaoCanoHorizontal,
+						alturaDipositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos / 2 + posicaoCanoVertical,
+						canoBaixo.getWidth(),
+						canoBaixo.getHeight()
+				);
 
 		retanguloCanoCima.set
-		(
-			posicaoCanoHorizontal,
-			alturaDipositivo /2 + espacoEntreCanos /2 + posicaoCanoVertical,
-			canoTopo.getWidth(),
-			canoTopo.getHeight()
-		);
+				(
+						posicaoCanoHorizontal,
+						alturaDipositivo /2 + espacoEntreCanos /2 + posicaoCanoVertical,
+						canoTopo.getWidth(),
+						canoTopo.getHeight()
+				);
 
 		boolean colidiuCanoCima  = Intersector.overlaps(circuloPassaro, retanguloCanoCima);
 		boolean colidiuCanoBaixo = Intersector.overlaps(circuloPassaro, retanguloCanoBaixo);
@@ -254,8 +276,11 @@ public class jogo extends ApplicationAdapter {
 
 		if(colidiuMoeda == true)
 		{
-			pontos += valorMoeda1;
+			if(moedaAtual == moeda1) pontos += valorMoeda1;
+			else pontos += valorMoeda2;
+
 			posicaoMoedaY = alturaDipositivo * 2;
+			somMoeda.play();
 		}
 
 		if (colidiuCanoBaixo || colidiuCanoCima)
@@ -268,43 +293,23 @@ public class jogo extends ApplicationAdapter {
 		}
 	}
 
-	private void desenharTexturas()
+	//
+	private void desenharCena()
 	{
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 
 		batch.draw(fundo,0,0,larguraDispositivo,alturaDipositivo);
-
 		batch.draw
 		(
 			passaros[(int) variacao],
-			50 + posicaoHorizontalPassaro,
+			posicaoHorizontalPassaro,
 			posicaoInicialVerticalPassaro,
-			passaros[(int) variacao].getWidth()/2,
-			passaros[(int) variacao].getHeight()/2
+			passaros[0].getWidth() * escalaPassaro,
+			passaros[0].getHeight() * escalaPassaro
 		);
 
-		textoPontuacao.draw
-		(
-			batch,
-			String.valueOf(pontos),
-			larguraDispositivo/ 2,
-			alturaDipositivo - 110
-		);
-
-		if(estadoJogo == 0 )
-		{
-			batch.draw
-			(
-				logo,
-				larguraDispositivo/2 - logo.getWidth()/2/4,
-				alturaDipositivo / 3,
-				logo.getWidth()/4,
-				logo.getHeight()/4
-			);
-		}
-
-		if(estadoJogo == 1)
+		if(estadoJogo != 0)
 		{
 			// Desenha Cano baixo
 			batch.draw
@@ -326,60 +331,81 @@ public class jogo extends ApplicationAdapter {
 			batch.draw
 			(
 				moedaAtual,
-				posicaoMoedaX - moedaAtual.getWidth() / 2 * escalamoeda,
-				posicaoMoedaY - moedaAtual.getWidth() / 2 * escalamoeda,
-				moedaAtual.getWidth() * escalamoeda,
-				moedaAtual.getHeight() * escalamoeda
-			);
-		}
-		if(estadoJogo == 2 )
-		{
-			// Desenha imagem gameover
-			batch.draw
-			(
-					gameOver,
-					larguraDispositivo/2 - gameOver.getWidth()/2,
-					alturaDipositivo / 2
-			);
-
-			// Desenha texto reiniciar
-			textoReiniciar.draw
-			(
-					batch,
-					"Toque para reiniciar!",
-					larguraDispositivo/2 -140,
-					alturaDipositivo/2 - gameOver.getHeight()/2
-			);
-
-			// Desenha high score
-			textoMelhorPontuacao.draw
-			(
-				batch,
-				"Seu Record é: " + pontuacaoMaxima + " pontos",
-				larguraDispositivo/2 -140,
-				alturaDipositivo/2 - gameOver.getHeight()
-			);
-
-			// Desenha cano baixo
-			batch.draw
-			(
-				canoBaixo,
-				posicaoCanoHorizontal,
-				alturaDipositivo / 2 - canoBaixo.getHeight() - espacoEntreCanos/2 + posicaoCanoVertical
-			);
-
-			// Desenha cano topo
-			batch.draw
-			(
-				canoTopo,
-				posicaoCanoHorizontal,
-				alturaDipositivo / 2 + espacoEntreCanos / 2 + posicaoCanoVertical
+				posicaoMoedaX - (moedaAtual.getWidth() * escalaMoeda),
+				posicaoMoedaY - (moedaAtual.getWidth() * escalaMoeda),
+				moedaAtual.getWidth() * escalaMoeda,
+				moedaAtual.getHeight() * escalaMoeda
 			);
 		}
 
 		batch.end();
 	}
 
+	//
+	private void desenharInterface()
+	{
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+
+		// Desenha pontuação
+		textoPontuacao.draw
+		(
+			batch,
+			String.valueOf(pontos),
+			larguraDispositivo/ 2,
+			alturaDipositivo - 110
+		);
+
+		switch (estadoJogo)
+		{
+			case 0:
+			{
+				// Desenha logo
+				batch.draw
+				(
+					logo,
+					larguraDispositivo/2 - logo.getWidth()/2/4,
+					alturaDipositivo / 3,
+					logo.getWidth()/4,
+					logo.getHeight()/4
+				);
+			}break;
+
+			case 1: {} break;
+
+			case 2:
+			{
+				// Desenha imagem gameover
+				batch.draw
+				(
+					gameOver,
+					larguraDispositivo/2 - gameOver.getWidth()/2,
+					alturaDipositivo / 2
+				);
+
+				// Desenha texto reiniciar
+				textoReiniciar.draw
+				(
+					batch,
+					"Toque para reiniciar!",
+					larguraDispositivo/2 -140,
+					alturaDipositivo/2 - gameOver.getHeight()/2
+				);
+
+				// Desenha high score
+				textoMelhorPontuacao.draw
+				(
+					batch,
+					"Seu Record é: " + pontuacaoMaxima + " pontos",
+					larguraDispositivo/2 -140,
+					alturaDipositivo/2 - gameOver.getHeight()
+				);
+			}break;
+		}
+		batch.end();
+	}
+
+	//
 	public void validarPontos()
 	{
 		if (posicaoCanoHorizontal < posicaoHorizontalPassaro)
@@ -400,12 +426,31 @@ public class jogo extends ApplicationAdapter {
 		}
 	}
 
+	//
+	private void resetaMoeda()
+	{
+		posicaoMoedaX = posicaoCanoHorizontal + canoBaixo.getWidth() + moedaAtual.getWidth() + random.nextInt((int) (larguraDispositivo - (moedaAtual.getWidth() * escalaMoeda)));
+		posicaoMoedaY = moedaAtual.getHeight() / 2 + random.nextInt((int) alturaDipositivo - moedaAtual.getHeight() / 2);
+
+		int randomMoedaNova = random.nextInt(100);
+		if(randomMoedaNova < 30)
+		{
+			moedaAtual = moeda2;
+		}
+		else
+		{
+			moedaAtual = moeda1;
+		}
+	}
+
+	//
 	@Override
 	public void resize(int width, int height)
 	{
 		viewport.update(width, height);
 	}
 
+	//
 	@Override
 	public void dispose () {}
 }
